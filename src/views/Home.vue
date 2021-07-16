379e20d3c320aeb258ee74bc75f87a1c95b8c64d
tasos <lazarou.ts@gmail.com>
2021-07-16 14:37:39 +0300

    first Commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,48 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main v-if="!loading">
+    <DataTitle :text="title" :dataDate="dataDate"/>
+  </main>
+  <main class="flex flex-col align-center justify-center text-center" v-else>
+    <div class="text-gray-500 text-3xl mt-10 mb-6">
+      Fetching Data
+    </div>
+    <img :src="loadingImage" class="w-24 m-auto" alt="">
+  </main>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+import DataTitle from '../components/DataTitle.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
-  }
+    DataTitle
+  },
+  data() {
+    return {
+      loading:true,
+      title:'Global',
+      dataDate:'',
+      stats:{},
+      countries:[],
+      loadingImage: require('../assets/hourglass.gif')
+    }
+  },
+  methods: {
+    async fetchCovidData(){
+      const res = await fetch('https://api.covid19api.com/summary')
+      const data = await res.json()
+      return data
+    }
+  },
+  async created() {
+    const data = this.fetchCovidData()
+    
+    this.dataDate = data.Date
+    this.stats = data.Global
+    this.countries = data.Countries
+    this.loading = false
+  },
 }
 </script>
